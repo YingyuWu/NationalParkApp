b03b233ea802e97ea8cd88a57f369eff2b8dc166
Wendy <yywucs@gmail.com>
2015-10-17 23:36:37 -0400

    pie chart added

--- AdventureTracks/main.php
+++ AdventureTracks/main.php
@@ -15,7 +15,6 @@ if($track_type == ''){
   $track_type = 1;
 }
 require_once('includes/db_conn.php');
-$query1 = "SELECT * FROM `AdventureTracksPoints` WHERE User_ID = '".$userid."' AND Track_Type = '".$track_type."'";
 $query2 = "SELECT * FROM `AdventureTracks` WHERE ID = '".$track_type."'";//Default Setting for index page
 $result = $dbc->query($query2);
 if(!$result){
@@ -29,7 +28,45 @@ if($result->num_rows > 0){
             $track_description = $row['Description'];  
         }
 }
+$query1 = "SELECT count(*) FROM `Location` INNER JOIN `AdventureTracksPoints` WHERE `AdventureTracksPoints`.Locat_ID=`Location`.ID AND `Location`.User_ID = '".$userid."' AND `AdventureTracksPoints`.Track_Type = '".$track_type."' AND `Location`.Pub_Or_Priva = '1' ORDER BY `Location`.ID";
+$query3 = "SELECT count(*) FROM `questions` INNER JOIN `Location`WHERE `questions`.Locat_ID=`Location`.ID AND `questions`.Question_Type='1' AND `questions`.Track_Type='".$track_type."' AND `Location`.User_ID = '".$userid."'";
+$query4 = "SELECT count(*) FROM `FillQuestions` INNER JOIN `Location`WHERE `FillQuestions`.Locat_ID=`Location`.ID AND `FillQuestions`.Question_Type='1' AND `FillQuestions`.Track_Type='".$track_type."' AND `Location`.User_ID = '".$userid."'";
+$query5 = "SELECT count(*) FROM `singleQuestions` INNER JOIN `Location`WHERE `singleQuestions`.Locat_ID=`Location`.ID AND `singleQuestions`.Question_Type='1' AND `singleQuestions`.Track_Type='".$track_type."' AND `Location`.User_ID = '".$userid."'";
+$query6 = "SELECT count(*) FROM `multiQuestions` INNER JOIN `Location`WHERE `multiQuestions`.Locat_ID=`Location`.ID AND `multiQuestions`.Question_Type='1' AND `multiQuestions`.Track_Type='".$track_type."' AND `Location`.User_ID = '".$userid."'";
+$query7 = "SELECT count(*) FROM `matchQuestions` INNER JOIN `Location`WHERE `matchQuestions`.Locat_ID=`Location`.ID AND `matchQuestions`.Question_Type='1' AND `matchQuestions`.Track_Type='".$track_type."' AND `Location`.User_ID = '".$userid."'";
+$query8 = "SELECT count(*) FROM `orderQuestions` INNER JOIN `Location`WHERE `orderQuestions`.Locat_ID=`Location`.ID AND `orderQuestions`.Question_Type='1' AND `orderQuestions`.Track_Type='".$track_type."' AND `Location`.User_ID = '".$userid."'";
+$query9 = "SELECT count(*) FROM `factInformation` INNER JOIN `Location`WHERE `factInformation`.Locat_ID=`Location`.ID AND `factInformation`.Question_Type='1' AND `factInformation`.Track_Type='".$track_type."' AND `Location`.User_ID = '".$userid."'";
+$result=$dbc->query($query1);
+$row = $result->fetch_assoc();
+$points=$row['count(*)'];
+$result=$dbc->query($query3);
+$row = $result->fetch_assoc();
+$num_text=$row['count(*)'];
+$result=$dbc->query($query4);
+$row = $result->fetch_assoc();
+$num_fill=$row['count(*)'];
+$result=$dbc->query($query5);
+$row = $result->fetch_assoc();
+$num_single=$row['count(*)'];
+$result=$dbc->query($query6);
+$row = $result->fetch_assoc();
+$num_multi=$row['count(*)'];
+$result=$dbc->query($query7);
+$row = $result->fetch_assoc();
+$num_match=$row['count(*)'];
+$result=$dbc->query($query8);
+$row = $result->fetch_assoc();
+$num_order=$row['count(*)'];
+$result=$dbc->query($query9);
+$row = $result->fetch_assoc();
+$num_fact=$row['count(*)'];
+$dbc->close();
 ?>
+<script type="text/javascript" src="js/canvasjs.min.js"></script>
+<script type="text/javascript" src="js/wow.js"></script>
+<script>
+    new WOW().init();//For speeding up the loading time of webpage
+</script>
 <script type="text/javascript">
 
 function init(){
@@ -43,7 +80,45 @@ function init(){
   document.getElementById("header-user-id").value = user_id;
   document.getElementById("header-role-id").value = role_id;
   document.getElementById("main-content").innerHTML = "<h1 class='description'><b>Description</b></h1>" + description;
+  chartRender();
   //document.getElementById("main-content").style.backgroundImage = "url('images/ledgesWeathering_background.png')";
+}
+
+function chartRender(){
+var points=<?php echo json_encode($points); ?>;
+var num_text=<?php echo json_encode($num_text); ?>;
+var num_fill=<?php echo json_encode($num_fill); ?>;
+var num_single=<?php echo json_encode($num_single); ?>;
+var num_multi=<?php echo json_encode($num_multi); ?>;
+var num_match=<?php echo json_encode($num_match); ?>;
+var num_order=<?php echo json_encode($num_order); ?>;
+var num_fact=<?php echo json_encode($num_fact); ?>;
+  var chart = new CanvasJS.Chart("chartContainer",
+  {
+    theme: "theme2",
+    title:{
+      text: "'Numbers in this track'"
+    },    
+    data: [
+    {       
+      type: "pie",
+      showInLegend: true,
+      toolTipContent: "{y}",
+      legendText: "{indexLabel}",
+      dataPoints: [
+        {  y: points, indexLabel: "points" },
+        {  y: num_text, indexLabel: "text/image questions" },
+        {  y: num_fill, indexLabel: "fill in questions" },
+        {  y: num_single, indexLabel: "single choice questions"},
+        {  y: num_multi, indexLabel: "multiple choices questions" },
+        {  y: num_match, indexLabel: "match questions"},
+        {  y: num_order, indexLabel: "correct order questions"},
+        {  y: num_fact, indexLabel: "information"}
+      ]
+    }
+    ]
+  });
+  chart.render();
 }
   
 </script>
@@ -58,9 +133,12 @@ function init(){
 <body onload="init()">
 <?php include('includes/left_menu.html'); ?>
 	<div class="wrappermiddle">
-		<div class="middle" id="main-content">
+		<div style="width:50%; float:left;border-right:1px solid grey" class="middle" id="main-content">
 
-	</div>    
+	</div>
+  <div style="width:50%;float:left" class="wow fadeIn" id="chartContainer">
+
+  </div>    
 	<!--<div class="right">
 	    Right column: 
 	     <br>
